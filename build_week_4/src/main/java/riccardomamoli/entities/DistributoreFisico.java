47bcb53f50a2856f51cd5e35511298c8e1e9ec6b
Nicolomecca <nico97.mecca@gmail.com>
2024-10-15 16:43:05 +0200

    up

--- build_week_4/src/main/java/riccardomamoli/entities/DistributoreFisico.java
+++ build_week_4/src/main/java/riccardomamoli/entities/DistributoreFisico.java
@@ -2,12 +2,18 @@ package riccardomamoli.entities;
 
 import jakarta.persistence.Entity;
 
+import java.util.List;
+
 @Entity
 
 
 public class DistributoreFisico extends PuntoVendita {
 
     public DistributoreFisico() {
+    }
+
+    public DistributoreFisico(List<Abbonamento> abbonamenti, List<Biglietto> biglietti) {
+        super(abbonamenti, biglietti);
     }
 
     @Override
