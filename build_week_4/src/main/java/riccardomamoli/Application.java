651f5704003a795d107fed902f6ecab0a5401fd1
lsodini <lucasodini5@gmail.com>
2024-10-15 15:00:08 +0200

    .

--- build_week_4/src/main/java/riccardomamoli/Application.java
+++ build_week_4/src/main/java/riccardomamoli/Application.java
@@ -16,5 +16,29 @@ public class Application {
 
         em.close();
         emf.close();
+
+
+                while (true) {
+                    try {
+                        System.out.println("Che tipo di utente sei? (1: Utente, 2: Amministratore) /n");
+                        int scelta = scanner.nextInt();
+                        scanner.nextLine();
+
+                        switch (scelta) {
+                            case 1: System.out.println("Benvenuto utente, seleziona il punto vendita:");
+
+
+                            do {
+                                int selezioneUtente = scanner.nextInt();
+                                switch (selezioneUtente) {
+                                    case 1:
+                                }
+                            }
+                            while(!selezioneUtente )
+                            }
+                            }
+
+                        }
+
     }
 }
